bc80ddce00b66a0786c88b0d044845befa9a46d3
KarinaRadkevich <Karina.radkevich.96@mail.ru>
2024-08-09 18:28:42 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { data } from './data';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [place,setPlace] = useState(0);
+  const {id, title, image} = data[place];
+
+  const deletePlace = (id) => {
+    let newPlaces = place.filter(item => item.id !== id);
+    setPlace(newPlaces);
+      }
+
+  const back = () => {
+    setPlace(place => {
+      place --;
+      if (place<0) {
+        place = data.length-1;
+      }
+      return place;
+    })
+  }
+
+  const next = () => {
+    setPlace(place => {
+      place ++;
+      if(place>data.length-1) {
+        place=0;
+      }
+      return place;
+    })
+  }
+ 
+
+  return(
+    <div>
+      <div className='container'>
+        <h1>TOP countries, which I want to visit:</h1>
+      </div>
+      <div className='container'>
+        <img src={image} width="550px" height="370px" alt="picture"/>
+      </div>
+      <div className='container'>
+        <h2>{id} - {title}</h2>
+      </div>
+      <div className='container'>
+        <button className='color' onClick={()=> deletePlace(id)}>Done!</button>
+      </div>
+      <div className='container btn'>
+        <button onClick={back}>Back</button>
+        <button onClick={next}>Next</button>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,39 @@
+export const data = [
+    {
+        id: 1,
+        title: "Switzerland",
+        image: "https://cdn.britannica.com/80/155980-050-3A532B36/Restaurant-village-Alpine-Grindelwald-Switzerland.jpg"
+    },
+
+    {
+        id: 2,
+        title: "America",
+        image: "https://cdn.britannica.com/61/93061-050-99147DCE/Statue-of-Liberty-Island-New-York-Bay.jpg"
+    },
+
+    {
+        id: 3,
+        title: "Spain",
+        image: "https://d3dqioy2sca31t.cloudfront.net/Projects/cms/production/000/020/484/large/d0531471711b367b94abfd4dbc29e6ae/spain-barcelona-sagrada-familia-080416-az.jpg"
+    },
+
+    {
+        id: 4,
+        title: "Maldives",
+        image: "https://www.hollywoodreporter.com/wp-content/uploads/2023/03/Maldives-ArrivalJetty-9-H-2023.jpg?w=1296&h=730&crop=1"
+    },
+
+    
+    {
+        id: 5,
+        title: "France",
+        image: "https://media.cnn.com/api/v1/images/stellar/prod/230324090551-01-visiting-france-during-protests-what-to-know-top.jpg?c=original"
+    },
+
+    {
+        id: 6,
+        title: "Indonesia",
+        image: "https://res.cloudinary.com/lastminute-contenthub/s--1FBiktHL--/c_limit,h_999999,w_1920/f_auto/q_auto:eco/v1/DAM/Photos/Destinations/Asia/Indonesia/Bali/shutterstock_380725909"
+    }
+
+]
